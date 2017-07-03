8319858e5bcd519facbbc08a970e773b07ca070f
Ashen One <Ashen One@MSI>
2017-07-03 14:08:04 +0100

    modification sur demandeCategory ; Create, List et View

---
+++ src/java/service/SortimentFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import bean.Sortiment;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Younes
+ */
+@Stateless
+public class SortimentFacade extends AbstractFacade<Sortiment> {
+
+    @PersistenceContext(unitName = "kt_FST_2PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SortimentFacade() {
+        super(Sortiment.class);
+    }
+    
+}
